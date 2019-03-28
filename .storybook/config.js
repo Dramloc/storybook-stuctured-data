6477ba590283aa116d299f879ebc5b1ff011f3d0
Antoine Colmard <a.colmard@gmail.com>
2019-03-29 00:27:12 +0100

    :construction_worker: Add storybook knobs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,12 @@
 import { addDecorator, addParameters, configure } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
+import { withKnobs } from '@storybook/addon-knobs';
 import requireContext from 'require-context.macro';
 
 const importAll = r => r.keys().forEach(r);
 const loadStories = () => importAll(requireContext('../src', true, /\.stories.jsx?$/));
 
+addDecorator(withKnobs);
 addDecorator(withA11y);
 addParameters({
   backgrounds: [{ name: 'default', value: '#ffffff', default: true }],
--- src/event/Event.stories.jsx
+++ src/event/Event.stories.jsx
@@ -1,29 +1,44 @@
 import centered from '@storybook/addon-centered/react';
 import { storiesOf } from '@storybook/react';
+import {
+  text, select, number, date as dateKnob,
+} from '@storybook/addon-knobs';
 import React from 'react';
 import { Event } from './Event';
+
+const date = (name, defaultValue) => {
+  const stringTimestamp = dateKnob(name, defaultValue);
+  return new Date(stringTimestamp);
+};
 
 storiesOf('Event', module)
   .addDecorator(centered)
   .add('default', () => (
     <Event
       event={{
-        name: 'Typhoon with Radiation City',
-        startDate: new Date('2019-03-28T22:47:35.292Z'),
+        name: text('Name', 'Typhoon with Radiation City'),
+        startDate: date('Date', new Date('2019-03-28T22:47:35.292Z')),
         location: {
-          name: 'The Hi-Dive',
+          name: text('Location Name', 'The Hi-Dive'),
           address: {
-            streetAddress: '7 S. Broadway',
-            addressLocality: 'Denver',
-            addressRegion: 'CO',
-            postalCode: '80209',
+            streetAddress: text('Street Address', '7 S. Broadway'),
+            addressLocality: text('Address Locality', 'Denver'),
+            addressRegion: text('Address Region', 'CO'),
+            postalCode: text('Postal Code', '80209'),
           },
         },
         offers: [
           {
-            url: 'http://www.ticketfly.com/purchase/309433',
-            price: 13.0,
-            priceCurrency: 'EUR',
+            url: text('URL', 'http://www.ticketfly.com/purchase/309433'),
+            price: number('Price', 13.0),
+            priceCurrency: select(
+              'Currency',
+              {
+                EUR: 'EUR',
+                USD: 'USD',
+              },
+              'EUR',
+            ),
           },
         ],
       }}
